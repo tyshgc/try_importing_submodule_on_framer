9a3195208cce5fb282d91e67460eba9df5913aee
tyshgc <hello@tyshgc.pw>
2020-05-28 20:27:38 +0900

    Added: Framer project files

---
+++ design.framerfx/code/canvas.tsx
@@ -0,0 +1,4 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Gui_parts_MemberListItem = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,icon?:string,name?:string}>(canvas, "id_UHmEtfxys", {icon:"image",name:"string"}, 316,69);
