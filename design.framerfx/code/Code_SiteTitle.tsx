38c2a125025f4a75f09b0a3e561933cdaca6b89a
tyshgc <hello@tyshgc.pw>
2020-05-29 12:14:00 +0900

    Added  CodeComponent

---
+++ design.framerfx/code/Code_SiteTitle.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { SiteTitle } from "../try_framer_with_submodule/src/components/gui/parts/";
+
+export function Code_SiteTitle() {
+  return <SiteTitle value="Framer" />;
+}
